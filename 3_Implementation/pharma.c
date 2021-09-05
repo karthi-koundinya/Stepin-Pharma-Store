84356444eb655c2f040c982764db49c84bdfaf92
karthi-koundinya <karthikarthik5566@gmail.com>
2021-09-05 11:13:22 +0530

    initial commit

--- 3_Implementation/pharma.c
+++ 3_Implementation/pharma.c
@@ -9,262 +9,306 @@
  * 
  */
 #include<stdio.h>
- #include<string.h>
- #include<ctype.h>
- struct Medicine{
-   int id,price,quantity;
-   char medicneName[100],Company[100],Mfg_Date[11],Exp_Date[11];
-  }m[100];
-
- void DisplayMedicine(int number);
- void SearchMedicine(int number);
- void AddMedicine(int number,struct Medicine m[]);
- void DeleteMedicine(int number);
- void UpdateMedicine(int number);
- int main()
- {
-   int i,j,choice,number=0,c;
-   for(i=0;i<100;i++)
-   {
-    m[i].id=0;
-   m[i].price=0;
-   m[i].quantity=0;
-   strcpy(m[i].Mfg_Date,"");
-  strcpy(m[i].Exp_Date,"");
-   strcpy(m[i].medicneName,"");
-   strcpy(m[i].Company,"");
-   }
-   m[0].id=1;
-   m[0].price=120;
-   m[0].quantity=30;
-   strcpy(m[0].Mfg_Date,"23-04-2021");
-  strcpy(m[0].Exp_Date,"24-04-2022");
-   strcpy(m[0].medicneName,"Paracetmol");
-   strcpy(m[0].Company,"JK Pharma");
-   do{
-   printf("Enter\n1 - Add Medicine\n2 - Update Medicine\n3 - Delete Medicine\n4 - Search Medicine\n5 - Display Medicine\n");
-   scanf("%d",&choice);
-   switch(choice)
-   {
-
-      case 1:
-      {
-       ++number;
-       AddMedicine(number,m);
-       break;
-      }
-      case 2:
-      {
-        UpdateMedicine(number+1);
-
-       break;
-      }
-      case 3:
-      {
-      DeleteMedicine(number+1);
-       break;
-      }
-	case 4:
-      {
-
-       SearchMedicine(number+1);
-
-       break;
-
-      }
-	case 5:
-
-      {
-
-       DisplayMedicine(number+1);
-
-       break;
-
-      }
-   }
-     printf("To Continue with other Options Enter 1 Else any other number\n");
-     scanf("%d",&c);
-   }while(c==1);
-
- }
-
- void SearchMedicine(int number){
-   int i,flag=0;
-  char name[100];
-  printf("Enter the Medicine Name\n");
-  fflush(stdin);
-  scanf("%s",name);
-  for(i=0;i<number;i++)
-  {
-   if(strcmp(m[i].medicneName,name)==0)
-   {
-    flag=1;
-    printf("These are the details of Medicine\n");
-   printf("\n==========================================================================================\n");
-    printf("Id\t Name\t\t Price\t Qnty\t Company\t Mfg Date\t Exp Date\n");
-printf("\n==========================================================================================\n");
-printf("%d\t %s\t %d\t %d\t %s\t %s\t %s\t",m[i].id,m[i].medicneName,m[i].price,m[i].quantity,m[i].Company,m[i].Mfg_Date,m[i].Exp_Date);
-printf("\n==========================================================================================\n");
-  
-   }
-  }
-  if(flag==0)
-  {
-   printf("Entered Name Not Found\n");
-  }
- }
- void DisplayMedicine(int number){
-  int i;
-  if(number!=0)
-  {
-      printf("All Available Items are\n");
-   for(i=0;i<number;i++)
-   {
-    if(m[i].id!=0){
-printf("\n==========================================================================================\n");
-    printf("Id\t Name\t\t Price\t Qnty\t Company\t Mfg Date\t Exp Date\n");
-printf("\n==========================================================================================\n");
-printf("%d\t %s\t %d\t %d\t %s\t %s\t %s\t",m[i].id,m[i].medicneName,m[i].price,m[i].quantity,m[i].Company,m[i].Mfg_Date,m[i].Exp_Date);
-printf("\n==========================================================================================\n");
-      }
-   }
-  }
-  else{
-   printf("\nNo Items or Medicines Available\n");
-  }
- }
- void AddMedicine(int number,struct Medicine m[])
- {
-  char name[100];
-  printf("Enter Medicine Id\n");
-  scanf("%d",&(m[number].id));
-  fflush(stdin);
-  printf("Enter Medicine Name\n");
-  scanf("%s",(m[number].medicneName));
-	fflush(stdin);
-  printf("Enter Company Name\n");
-  fflush(stdin);
- scanf("%s",(m[number].Company));
-  printf("Enter Manufactured Date\n");
-  fflush(stdin);
- scanf("%s",(m[number].Mfg_Date));
-  printf("Enter Expiry Date\n");
-  fflush(stdin);
-  scanf("%s",(m[number].Exp_Date));
-  printf("Enter Quantity\n");
-  fflush(stdin);
-  scanf("%d",&(m[number].quantity));
-  printf("Enter Price\n");
-  fflush(stdin);
-  scanf("%d",&(m[number].price));
-  printf("Medicine with id %d Added Successfully\n",m[number].id);
- }
-
- void DeleteMedicine(int number)
- {
-  int id,i,flag=0,num;
-  printf("Enter Id to be deleted\n");
-  fflush(stdin);
-  scanf("%d",&id);
-  for(i=0;i<number;i++)
-  {
-   if(m[i].id==id)
-   {
-    flag=1;
-    m[i].id=0;
-    m[i].price=0;
-    m[i].quantity=0;
-    strcpy(m[i].medicneName,"");
-    strcpy(m[i].Company,"");
-    strcpy(m[i].Mfg_Date,"");
-    strcpy(m[i].Exp_Date,"");
-    num=i;
-    break;
-   }
-  }
-  if(flag==1)
-  {
-   printf("Medicine with %d is Deleted Successfully\n",id);
-  }
- }
-
- void UpdateMedicine(int number)
- {
-  int id,quantity,choice,c,i;
-  printf("Enter id to change Details\n");
-  scanf("%d",&id);
-  for(i=0;i<number;i++)
-  {
-   if(m[i].id==id && m[i].id!=0)
-   {
-    do
+#include<conio.h>
+#include<stdlib.h>
+#include<windows.h>
+#include<string.h>
+void gotoxy(int ,int );
+void menu();
+void add();
+void display();
+void search();
+void modify();
+void delete();
+typedef struct medicine
+{
+    char name[20];
+    char cname[10];
+    int price;
+    char mfg_date[20];
+    char exp_date[20];
+}m;
+int main()
+{
+    gotoxy(15,8);
+    printf("<--:Pharma Store Management System:-->");
+    gotoxy(19,15);
+    printf("\nPress any key to continue.");
+    getch();
+    menu();
+    return 0;
+}
+void menu()
+{
+    int choice;
+    system("cls");
+    gotoxy(10,3);
+    printf("<--:MENU:-->");
+    gotoxy(10,5);
+    printf("Enter appropriate number to perform following task.");
+    gotoxy(10,7);
+    printf("1 : Add Record.");
+    gotoxy(10,8);
+    printf("2 : Display Record.");
+    gotoxy(10,9);
+    printf("3 : Search Record.");
+    gotoxy(10,10);
+    printf("4 : Modify Record.");
+    gotoxy(10,11);
+    printf("5 : Delete Record.");
+    gotoxy(10,12);
+    printf("6 : Exit.");
+    gotoxy(10,13);
+    printf("Enter your choice.");
+    scanf("%d",&choice);
+    switch(choice)
     {
-        printf("Enter\n1 - Change Quantity\n2 - Change Price\n3 - Change Name\n4 - Change Company\n5 - Change Manufaturing Date\n6 - Change Expiry Date\nAny other number to exit\n");
-        scanf("%d",&choice);
-        if(choice==1)
-        {
-         int quantity;
-         printf("Enter Quantity to be update\n");
-         fflush(stdin);
-         scanf("%d",&quantity);
-         m[i].quantity=quantity;
-         printf("Quantity updated Successfully\n");
-        }
-        if(choice==2)
-        {
-         int price;
-         printf("Enter Price to be update\n");
-         fflush(stdin);
-         scanf("%d",&price);
-         m[i].price=price;
-         printf("Price updated Successfully\n");
-        }
-        if(choice==3)
-        {
-         char name[100];
-         printf("Enter Name to be update\n");
-         fflush(stdin);
-         scanf("%s",name);
-         strcpy(m[i].medicneName,name);
-         printf("Medicine Name updated Successfully\n");
-        }
-        if(choice==4)
-        {
-         char company[100];
-         printf("Enter company to be update\n");
-         fflush(stdin);
-         scanf("%s",company);
-         strcpy(m[i].Company,company);
-         printf("Company updated Successfully\n");
-        }
-        if(choice==5)
-        {
-         char mfg[11];
-         printf("Enter Manufacturing date to be updatd\n");
-         fflush(stdin);
-         scanf("%s",mfg);
-         strcpy(m[i].Mfg_Date,mfg);
-         printf("Manufacturing Date updated Successfully\n");
-        }
-        if(choice==6)
-        {
-         char exp[11];
-         printf("Enter Expiry date to be update\n");
-         fflush(stdin);
-         scanf("%s",exp);
-         strcpy(m[i].Exp_Date,exp);
-         printf("Expiry Date updated Successfully\n");
-        }
-        if(choice<=0 && choice>6)
-        {
-         printf("Enter valid Choice\n");
-        }
-        printf("Enter 1 to Change other Details Else any other number\n");
-        fflush(stdin);
-        scanf("%d",&c);
-    }while(c==1);
+    case 1:
+        add();
+        break;
 
-    break;
-   }
-  }
- }
+    case 2:
+        display();
+        break;
+
+    case 3:
+        search();
+        break;
+
+    case 4:
+        modify();
+        break;
+
+    case 5:
+        delete();
+        break;
+
+    case 6:
+        exit(1);
+        break;
+
+    default:
+        gotoxy(10,17);
+        printf("Invalid Choice.");
+    }
+}
+void add()
+{
+    FILE *fp;
+    struct medicine m;
+    char another ='y';
+    system("cls");
+
+    fp = fopen("record.txt","ab+");
+    if(fp == NULL){
+        gotoxy(10,5);
+        printf("Error opening file");
+        exit(1);
+    }
+    fflush(stdin);
+    while(another == 'y')
+    {
+        gotoxy(10,3);
+        printf("<--:ADD RECORD:-->");
+        gotoxy(10,5);
+        printf("Enter details of medicine.");
+        gotoxy(10,7);
+        printf("Enter Name : ");
+//        gets(std.name);///???
+        gets(m.name);
+        gotoxy(10,8);
+        printf("Enter Company Name  : ");
+        gets(m.cname);
+        gotoxy(10,9);
+        printf("Enter Price : ");
+        scanf("%d",&m.price);
+        fflush(stdin);
+        gotoxy(10,10);
+        printf("Enter Manufacture Date : ");
+//        gets(std.mfg_date);///???
+        gets(m.mfg_date);
+        gotoxy(10,11);
+        printf("Enter Expiry Date : ");
+        gets(m.exp_date);
+//        gotoxy(10,12);
+//        printf("Enter Father's Name : ");
+//        gets(std.fathername);
+        fwrite(&m,sizeof(m),1,fp);
+        gotoxy(10,15);
+        printf("Want to add of another record? Then press 'y' else 'n'.");
+        fflush(stdin);
+//        another = getch();///???
+        another = getch();
+        system("cls");
+        fflush(stdin);
+    }
+    fclose(fp);
+    gotoxy(10,18);
+    printf("Press any key to continue.");
+    getch();
+    menu();
+}
+void display()
+{
+    FILE *fp;
+    int i=1,j;
+    struct medicine m;
+    system("cls");
+    gotoxy(10,3);
+    printf("<--:Display RECORD:-->");
+    gotoxy(10,5);
+    printf("S.No   Name of medicine   Company Name  Price      mfg_date     exp_date");
+    gotoxy(10,6);
+    printf("===========================================================================");
+    fp = fopen("record.txt","rb+");
+    if(fp == NULL){
+        gotoxy(10,8);
+        printf("Error opening file.");
+        exit(1);
+    }
+    j=8;
+    while(fread(&m,sizeof(m),1,fp) == 1){
+        gotoxy(10,j);
+        printf("%-7d%-22s%-12s%-10d%-13s%-13s",i,m.name,m.cname,m.price,m.mfg_date,m.exp_date);
+        i++;
+        j++;
+    }
+    fclose(fp);
+    gotoxy(10,j+3);
+    printf("Press any key to continue.");
+    getch();
+    menu();
+}
+void search()
+{
+    FILE *fp;
+    struct medicine m;
+    char stname[20];
+    system("cls");
+    gotoxy(10,3);
+    printf("<--:SEARCH RECORD:-->");
+    gotoxy(10,5);
+    printf("Enter name of medicine : ");
+    fflush(stdin);
+    gets(stname);
+    fp = fopen("record.txt","rb+");
+    if(fp == NULL){
+        gotoxy(10,6);
+        printf("Error opening file");
+        exit(1);
+    }
+    while(fread(&m,sizeof(m),1,fp ) == 1){
+        if(strcmp(stname,m.name) == 0){
+            gotoxy(10,8);
+            printf("Name : %s",m.name);
+            gotoxy(10,9);
+            printf("Company Name : %s",m.cname);
+            gotoxy(10,10);
+            printf("Price : %d",m.price);
+            gotoxy(10,11);
+            printf("Manufacture Date : %s",m.mfg_date);
+            gotoxy(10,12);
+            printf("Expiry Date : %s",m.exp_date);
+        }
+    }
+    fclose(fp);
+    gotoxy(10,16);
+    printf("Press any key to continue.");
+    getch();
+    menu();
+}
+void modify()
+{
+    char stname[20];
+    FILE *fp;
+    struct medicine m;
+    system("cls");
+    gotoxy(10,3);
+    printf("<--:MODIFY RECORD:-->");
+    gotoxy(10,5);
+    printf("Enter name of medicine to modify: ");
+    fflush(stdin);
+    gets(stname);
+    fp = fopen("record.txt","rb+");
+    if(fp == NULL){
+        gotoxy(10,6);
+        printf("Error opening file");
+        exit(1);
+    }
+    rewind(fp);
+    fflush(stdin);
+    while(fread(&m,sizeof(m),1,fp) == 1)
+    {
+        if(strcmp(stname,m.name) == 0){
+            gotoxy(10,7);
+            printf("Enter Name: ");
+            gets(m.name);
+            gotoxy(10,8);
+            printf("Enter Company Name  : ");
+            gets(m.cname);
+            gotoxy(10,9);
+            printf("Enter Price : ");
+            scanf("%d",&m.price);
+            gotoxy(10,10);
+            printf("Enter Manufacture Date : ");
+            fflush(stdin);
+            gets(m.mfg_date);
+            gotoxy(10,11);
+            printf("Enter Expiry Date : ");
+            fflush(stdin);
+            gets(m.exp_date);
+            fseek(fp ,-sizeof(m),SEEK_CUR);
+            fwrite(&m,sizeof(m),1,fp);
+            break;
+        }
+    }
+    fclose(fp);
+    gotoxy(10,16);
+    printf("Press any key to continue.");
+    getch();
+    menu();
+}
+void delete()
+{
+    char stname[20];
+    FILE *fp,*ft;
+    struct medicine m;
+    system("cls");
+    gotoxy(10,3);
+    printf("<--:DELETE RECORD:-->");
+    gotoxy(10,5);
+    printf("Enter name of medicine to delete record : ");
+    fflush(stdin);
+    gets(stname);
+    fp = fopen("record.txt","rb+");
+    if(fp == NULL){
+        gotoxy(10,6);
+        printf("Error opening file");
+        exit(1);
+    }
+    ft = fopen("temp.txt","wb+");
+    if(ft == NULL){
+        gotoxy(10,6);
+        printf("Error opening file");
+        exit(1);
+    }
+    while(fread(&m,sizeof(m),1,fp) == 1){
+        if(strcmp(stname,m.name)!=0)
+            fwrite(&m,sizeof(m),1,ft);
+    }
+    fclose(fp);
+    fclose(ft);
+    remove("record.txt");
+    rename("temp.txt","record.txt");
+    gotoxy(10,10);
+    printf("Press any key to continue.");
+    getch();
+    menu();
+}
+void gotoxy(int x,int y)
+{
+        COORD c;
+        c.X=x;
+        c.Y=y;
+        SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),c);
+}
