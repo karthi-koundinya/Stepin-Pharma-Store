cafe4e1561e637aff8c24be2cc611926be8e3367
karthi-koundinya <karthikarthik5566@gmail.com>
2021-09-05 17:58:35 +0530

    initial commit

---
+++ 3_Implementation/inc/pharma_operations.h
@@ -0,0 +1,20 @@
+
+#ifndef __PHARMA_OPERATIONS_H__
+#define __PHARMA_OPERATIONS_H__
+
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+#include<windows.h>
+#include<string.h>
+
+
+void gotoxy(int ,int );
+void menu();
+void add();
+void display();
+void search();
+void modify();
+void deleterec();
+
+#endif  
