6ef8e71a1ca2376d56e1c838b519df4c97b6f85a
karthi-koundinya <karthikarthik5566@gmail.com>
2021-09-06 17:24:31 +0530

    initial commit

--- 3_Implementation/inc/pharma_operations.h
+++ 3_Implementation/inc/pharma_operations.h
@@ -1,20 +1,30 @@
-
+/**
+ * @file pharma_operations.h
+ * @author Karthik S (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-09-06
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 #ifndef __PHARMA_OPERATIONS_H__
 #define __PHARMA_OPERATIONS_H__
 
 #include<stdio.h>
- #include<string.h>
- #include<ctype.h>
+#include<string.h>
+#include<ctype.h>
 
-struct Medicine{
+struct Medicine
+{
    int id,price,quantity;
    char medicneName[100],Company[100],Mfg_Date[11],Exp_Date[11];
-  }m[100];
+}m[100];
 
 void DisplayMedicine(int number);
- void SearchMedicine(int number);
- void AddMedicine(int number,struct Medicine m[]);
- void DeleteMedicine(int number);
- void UpdateMedicine(int number);
+void SearchMedicine(int number);
+void AddMedicine(int number,struct Medicine m[]);
+void DeleteMedicine(int number);
+void UpdateMedicine(int number);
 
 #endif  /* __PHARMA_OPERATIONS_H__*/
