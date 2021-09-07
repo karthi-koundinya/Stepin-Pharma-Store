5a86a6e83ce330a4234e51e026382353a84903e0
karthi-koundinya <karthikarthik5566@gmail.com>
2021-09-07 12:44:11 +0530

    initial commit

--- 3_Implementation/test/pharma_test.c
+++ 3_Implementation/test/pharma_test.c
@@ -17,25 +17,25 @@ void setUp(){}
 void tearDown(){}
 
 void test_add_medicine(void) {
-  number new_medicine = {"Add_1", "mname", "1234", "1234", "cname", "12-12-1234", "12-12-1234"};
+  number AddMedicine = {"Add_1", "mname", "1234", "1234", "cname", "12-12-1234", "12-12-1234"};
   TEST_ASSERT_EQUAL(0, add_medicine(&new_medicine));
 }
 
 void test_delete_medicine(void) {
-  number new_medicine = {"Delete_1", "mname", "1234", "1234", "cname", "12-12-1234", "12-12-1234"};
+  m new_medicine = {"Delete_1", "mname", "1234", "1234", "cname", "12-12-1234", "12-12-1234"};
   TEST_ASSERT_EQUAL(0, add_medicine(&new_medicine));
   TEST_ASSERT_EQUAL(0, delete_medicine(new_medicine.name));
 }
 
 void test_modify_medicine(void) {
-  number new_medicine = {"Modify_1", "mname", "1234", "1234", "cname", "12-12-1234", "12-12-1234"};
-  number new_medicine1 = {"Modified_id", "mname1", "123", "123", "cname1", "11-11-1235", "11-10-1235"};
+  m new_medicine = {"Modify_1", "mname", "1234", "1234", "cname", "12-12-1234", "12-12-1234"};
+  m new_medicine1 = {"Modified_id", "mname1", "123", "123", "cname1", "11-11-1235", "11-10-1235"};
   TEST_ASSERT_EQUAL(0, add_medicine(&new_medicine));
   TEST_ASSERT_EQUAL(0, modify_medicine(new_medicine.name, &new_medicine1));
   TEST_ASSERT_EQUAL(0, delete_medicine(new_medicine1.name));
 }
 void test_search_medicine(void) {
-  number new_medicine = {"Search_1", "mname", "1234", "1234", "cname", "12-12-1234", "12-12-1234"};
+  m new_medicine = {"Search_1", "mname", "1234", "1234", "cname", "12-12-1234", "12-12-1234"};
   TEST_ASSERT_EQUAL(0, add_medicine(&new_medicine));
   TEST_ASSERT_EQUAL(0 , search_medicine(new_medicine.name));
   TEST_ASSERT_EQUAL(0 , delete_medicine(new_medicine.name));
